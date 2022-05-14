5d1b8a5872d25b116d8812eb933411557d08a5a0
Mikaella Mae Silva <mikaellamaecsilva@gmail.com>
2022-05-14 19:14:51 +0800

    first commit

---
+++ PDC03_MOD03/PDC03_MOD03/MainPage.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace PDC03_MOD03
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        public async void Activity01(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new activity1());
+        }
+
+        public async void Activity02(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new activity2());
+        }
+
+        public async void Activity03(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new activity3());
+        }
+
+        public async void Activity04(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new activity4());
+        }
+
+        public async void Activity05(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new activity5());
+        }
+
+        public async void Activity06(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new activity6());
+        }
+    }
+}
---
+++ PDC03_MOD03/PDC03_MOD03/activity4.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PDC03_MOD03
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class activity4 : ContentPage
+    {
+        public activity4()
+        {
+            InitializeComponent();
+        }
+        async void OnButtonClicked(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            await DisplayAlert("Registered Voter", "You are a now registered voter located in Brgy. Duale, Limay, Bataan. Please vote wisely!", "OK");
+        }
+    }
+}
